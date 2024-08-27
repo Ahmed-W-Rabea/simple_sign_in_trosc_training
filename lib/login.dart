012d4965246cc2a90a602b19a9b08c1d9c954008
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2024-08-27 18:32:58 +0300

    'ui_sign_in_with_assets_and_obscureText'

---
+++ lib/login.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Login extends StatefulWidget {
+  Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image(
+          image: AssetImage("assets/back_ui.png"),
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        Center(
+          child: Container(
+            width: 320,
+            height: 500,
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 217, 223, 204),
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromARGB(195, 0, 0, 0),
+                    spreadRadius: 12,
+                    blurRadius: 12,
+                  ),
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Scaffold(
+                resizeToAvoidBottomInset: false,
+                backgroundColor: Colors.transparent,
+                body: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset("assets/logo_ui.png"),
+                        Text(
+                          "Sign in",
+                          style: GoogleFonts.jomolhari(fontSize: 32),
+                        ),
+                        SizedBox(
+                          width: 32,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 80,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "email",
+                          hintText: "AhmedWael@example.com"),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    TextFormField(
+                      obscureText: obscureText,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "password",
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                obscureText = !obscureText;
+                              });
+                            },
+                            icon: Icon(obscureText
+                                ? Icons.visibility
+                                : Icons.visibility_off)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
